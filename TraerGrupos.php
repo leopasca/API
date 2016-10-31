13030ddc4ad15f6bcac2c6e57f3920a02b9d6acd
leopas <leopasca@hotmail.com>
2016-10-31 20:49:37 -0300

    traerGrupos

---
+++ TraerGrupos.php
@@ -0,0 +1,31 @@
+<?php
+$con=mysqli_connect("localhost","u643932110_leop","leopas98","u643932110_leop");
+if(mysqli_connect_errno())
+{
+	echo "Failed to connect to MySQL: " .mysqli_connect_error();
+}
+else
+{
+
+$string = file_get_contents('php://input');
+$comentario=json_decode($string,true);
+$query = "SELECT * FROM Grupos";
+$result = mysqli_query($con, $query);
+$objetos = array();
+while($row = mysqli_fetch_array($result)) 
+{ 
+		$IdGrupo=$row['IdGrupo'];
+		$Nombre=$row['Nombre'];
+		$IdIntegrantes=$row['IdIntegrantes'];		
+    	$objeto = array('IdGrupo'=> $IdGrupo,'Nombre'=> $Nombre,'IdIntegrantes'=> $IdIntegrantes);	
+    	$objetos[] = $objeto;
+
+}
+
+mysqli_close($con);
+
+header("Content-Type: application/json");
+$json_string = json_encode($objetos,JSON_PRETTY_PRINT);
+echo $json_string;
+}
+?>
