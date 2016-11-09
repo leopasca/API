6fd76f9ff2a5bc511542266add4d10cc8ec2ba7a
GUIDO ALEJANDRO ALONGI <41835516@ort>
2016-11-09 09:00:14 -0300

    asdf

---
+++ TraerIntegrantesAgregar.php
@@ -0,0 +1,43 @@
+<?php
+$con=mysqli_connect("localhost","u643932110_leop","leopas98","u643932110_leop");
+if(mysqli_connect_errno())
+{
+	echo "Failed to connect to MySQL: " .mysqli_connect_error();
+}
+else
+{
+
+$string = file_get_contents('php://input');
+$comentario=json_decode($string,true);
+$query = "SELECT * From Usuarios 
+Where IdUsuario NOT IN (SELECT IdUsuario From UsuariosGrupo  Where IdGrupo = 3)
+";
+$stmt=$con->prepare($query);
+$stmt->bind_param(
+'i',
+$objeto["IdGrupo"]
+);
+$result = mysqli_query($con, $query);
+$objetos = array();
+while($row = mysqli_fetch_array($result)) 
+{ 
+		
+		$IdUsuario=$row['IdUsuario'];
+		$Email=$row['Email'];
+		$Password=$row['Password'];
+		$Nombre=$row['Nombre'];
+		$IdGrupo=$row['IdGrupo'];		
+		
+    	$objeto = array('IdUsuario'=> $IdUsuario,'Email'=> $Email,'Password'=> $Password,'Nombre'=> $Nombre,'IdGrupo'=> $IdGrupo);	
+    	$objetos[] = $objeto;
+
+
+}
+
+mysqli_close($con);
+
+header("Content-Type: application/json");
+$json_string = json_encode($objetos,JSON_PRETTY_PRINT);
+echo $json_string;
+}
+?>
